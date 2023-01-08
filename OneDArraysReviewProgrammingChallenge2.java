5cff28745ce7e9e92ab43d7e1ec5742f4fd9e8cd
Nikhil-Shyam <115193450+Nikhil-Shyam@users.noreply.github.com>
2023-01-08 15:40:30 -0500

    Update OneDArraysReviewProgrammingChallenge2.java

--- OneDArraysReviewProgrammingChallenge2.java
+++ OneDArraysReviewProgrammingChallenge2.java
@@ -1,12 +1,36 @@
 public class OneDArraysReviewProgrammingChallenge2
 {
+	static int[] arr;
+
 	public OneDArraysReviewProgrammingChallenge2(){
-		int[] arr = new int[50];
+		arr = new int[50];
 		for (int i = 0; i < arr.length; i++){
 			arr[i] = (int)(Math.random()*100)+1;
 		}
 
-		//1
+		problem1();
+
+		problem2();
+
+		problem3();
+
+		System.out.println("Array contains the number 27: " + problem4());
+
+		System.out.println("All numbers are greater than 10 in the array: " + problem5() + "\n");
+
+		for (int i = 0; i < arr.length-1; i++){
+			System.out.print(arr[i] + " ");
+		} System.out.println("\n");
+
+
+		problem6(); System.out.println("\n");
+
+		problem7(); System.out.println("\n");
+
+		problem8();
+	}
+
+	public void problem1(){
 		int max = Integer.MIN_VALUE;
 		int min = Integer.MAX_VALUE;
 		for (int i: arr){
@@ -16,8 +40,9 @@ public class OneDArraysReviewProgrammingChallenge2
 				max = i;
 		}
 		System.out.println("Max: " + max + "\nMin: " + min);
+	}
 
-		//2
+	public void problem2(){
 		int sum = 0;
 		int mode = 0;
 		int highCount = 0;
@@ -35,8 +60,67 @@ public class OneDArraysReviewProgrammingChallenge2
 		}
 		double avg = sum / (double)arr.length;
 		System.out.println("Sum: " + sum + "\nAverage: " + avg + "\nMode: " + mode);
+	}
 
-		//3
+	public void problem3(){
+		int ran = (int)(Math.random()*100)+1;
+		int count = 0;
+		for (int i: arr){
+			if (i == ran)
+				count++;
+		}
+		System.out.println("The number " + ran + " appears " + count + " times in the array");
+	}
+
+	public boolean problem4(){
+		for (int i: arr){
+			if (i == 27)
+				return true;
+		}
+		return false;
+	}
+
+	public boolean problem5(){
+		for (int i: arr){
+			if (i < 10)
+				return false;
+		}
+		return true;
+	}
+
+	public void problem6(){
+		for (int i = 0; i < 3; i++) {
+			int temp = arr[arr.length - 1];
+			for (int j = arr.length - 1; j > 0; j--)
+				arr[j] = arr[j - 1];
+			arr[0] = temp;
+		}
+		for (int i = 0; i < arr.length-1; i++){
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+	public void problem7(){
+		for (int i = 0; i < 5; i++) {
+			int temp = arr[0];
+			for (int j = 0; j < arr.length - 1; j++)
+				arr[j] = arr[j + 1];
+			arr[arr.length - 1] = temp;
+		}
+		for (int i = 0; i < arr.length-1; i++){
+			System.out.print(arr[i] + " ");
+		}
+	}
+
+	public void problem8(){
+		for (int i = 0; i < arr.length/2; i++){
+			int temp = arr[i];
+			arr[i] = arr[arr.length-i-1];
+			arr[arr.length-i-1] = temp;
+		}
+		for (int i = 0; i < arr.length-1; i++){
+			System.out.print(arr[i] + " ");
+		}
 	}
 
 	public static void main(String[] args){
