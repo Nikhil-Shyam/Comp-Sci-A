3db4ee66ad7870061b59a6da1ee3c7270b435d7a
Nikhil-Shyam <115193450+Nikhil-Shyam@users.noreply.github.com>
2023-05-17 23:06:57 -0400

    Delete NormalCitizen.java

--- NormalCitizen.java
+++
@@ -1,6 +0,0 @@
-public class NormalCitizen extends Person
-{
-	public NormalCitizen(String name, String job, int hitPoints, int maxDamage, int defenseAbility){
-		super(name, job, hitPoints, maxDamage, defenseAbility);
-	}
-}
