f0a4f7d196f870a66cebde0d37c02ee0d9e6e691
Nikhil-Shyam <115193450+Nikhil-Shyam@users.noreply.github.com>
2023-06-12 10:50:35 -0400

    Update Projectile.java

--- Final Project/Projectile.java
+++ Final Project/Projectile.java
@@ -2,7 +2,7 @@ public class Projectile
 {
 	private int xPosition;
 	private int yPosition;
-	static final int SPEED = 6;
+	static final int SPEED = (int)(Math.random()*6)+5;
 
 	public Projectile(int xPosition, int yPosition){
 		this.xPosition = xPosition;
