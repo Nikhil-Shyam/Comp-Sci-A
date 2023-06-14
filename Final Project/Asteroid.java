b0da90b1f83e9e10409a520ec721867f9bc40a99
Nikhil-Shyam <115193450+Nikhil-Shyam@users.noreply.github.com>
2023-06-14 18:50:46 -0400

    Update Asteroid.java

--- Final Project/Asteroid.java
+++ Final Project/Asteroid.java
@@ -6,6 +6,9 @@ public class Asteroid
 	private int asteroidY = 0;
 	private boolean isDestroyed = false;
 	private JComponent component;
+	private int count = 0;
+	private int velX = 2;
+	private int value = 1;
 
 	public Asteroid(JComponent component){
 		this.component = component;
@@ -30,6 +33,14 @@ public class Asteroid
 	public void updateAsteroid(){
 		if (!isDestroyed){
 			asteroidY += (int)(Math.random()*9)+2;
+			if (value == (int)(Math.random()*25)+1){
+				count++;
+				asteroidX += velX;
+				if (asteroidX > component.getWidth()-50 || asteroidX < 0 || count == 50){
+					velX = -velX;
+					count = 0;
+				}
+			}
 			if (asteroidY > component.getHeight()){
 				asteroidY = 0;
 				asteroidX = (int)(Math.random()*(component.getWidth()+1-50));
